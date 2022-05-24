2418aa033c48894cec094567a393f606a8eb7216
Brodie Peif <brodiep21@hotmail.com>
2022-05-24 15:37:11 -0400

    minor change, customizing site

--- main.go
+++ main.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	googlesearch "github.com/rocketlaunchr/google-search"
+)
+
+func init() {
+	templ = template.Must(template.ParseGlob("website/*.html"))
+}
+
+func SearchTax(w http.ResponseWriter, r *http.Request) {
+
+	response := r.FormValue("tax")
+
+	search, err := googlesearch.Search(context.TODO(), "tax rate of "+response)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	// client := http.Get()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Setting default port to %s", port)
+	}
+	fmt.Printf("Starting server at %s", port)
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/weather", HTMLresponse)
+	http.ListenAndServe(":"+port, nil)
 
 }
+
+// func
