bc3ab2066bacc2fc9d8c5df3c480401ecae8d938
Joshua Swick <joshuaswickirl@gmail.com>
2022-05-26 19:50:41 -0600

    Just a nudge

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -18,44 +19,6 @@ func init() {
 	templ = template.Must(template.ParseGlob("*.html"))
 }
 
-func LandingPage(w http.ResponseWriter, r *http.Request) {
-	templ.ExecuteTemplate(w, "index.html", nil)
-}
-
-var Tax string
-
-func SearchTax(w http.ResponseWriter, r *http.Request) {
-
-	response := r.FormValue("tax")
-
-	fmt.Println(response)
-	if r.Method != "POST" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	search, err := googlesearch.Search(context.TODO(), "tax rate of "+response)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(search)
-
-	for _, v := range search {
-		if strings.Contains(v.Description, "%") {
-			pos := strings.Index(v.Description, "%")
-			if pos == -1 {
-				fmt.Println("Could not locate Tax")
-			}
-			Tax = v.Description[(pos - 5) : pos+1]
-			Tax = strings.TrimPrefix(Tax, "is ")
-			Tax = strings.TrimPrefix(Tax, " ")
-			fmt.Println(Tax)
-			break
-		}
-	}
-	LandingPage(w, r)
-}
-
 func main() {
 
 	port := os.Getenv("PORT")
@@ -64,16 +27,61 @@ func main() {
 		log.Printf("Setting default port to %s", port)
 	}
 
-	http.HandleFunc("/", LandingPage)
-	http.HandleFunc("/test", SearchTax)
-	fmt.Println(Tax)
+	http.HandleFunc("/", theOneHandler)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 
-	fs := http.FileServer(http.Dir("assets/"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.Handle("/test/assets/", http.StripPrefix("/test/assets/", fs))
 	http.ListenAndServe(":"+port, nil)
-	fmt.Printf("Starting server at %s\n", port)
-
 }
 
-// func
+func theOneHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		get(w, r)
+	case http.MethodPost:
+		post(w, r)
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "index.html", nil)
+}
+
+type Data struct {
+	Tax string
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	zipCode := r.FormValue("tax")
+	tax, err := searchForTaxRate(zipCode)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	data := Data{
+		Tax: tax,
+	}
+	templ.ExecuteTemplate(w, "index.html", data)
+}
+
+func searchForTaxRate(zipCode string) (string, error) {
+
+	search, err := googlesearch.Search(context.TODO(), "tax rate of "+zipCode)
+	if err != nil {
+		return "", err
+	}
+
+	for _, v := range search {
+		if strings.Contains(v.Description, "%") {
+			pos := strings.Index(v.Description, "%")
+			if pos == -1 {
+				break
+			}
+			tax := v.Description[(pos - 5) : pos+1]
+			tax = strings.TrimPrefix(tax, "is ")
+			tax = strings.TrimPrefix(tax, " ")
+			return tax, nil
+		}
+	}
+
+	return "", errors.New("could not locate tax")
+}
